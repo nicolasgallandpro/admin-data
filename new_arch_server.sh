52f2dfe6351158b4d2bb18cf37a45cc40c85f8e2
Nicolas Galland <61990668+nicolasgallandpro@users.noreply.github.com>
2021-04-15 02:57:04 +0200

    ajout de nano

--- new_arch_server.sh
+++ new_arch_server.sh
@@ -1,4 +1,4 @@
 pacman -Sy
-pacman -S --noconfirm git docker docker-compose caddy curl tree starship
+pacman -S --noconfirm git docker docker-compose caddy curl tree starship nano
 git config --global init.defaultBranch main
 systemctl start docker
