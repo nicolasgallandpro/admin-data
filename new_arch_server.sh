4b9eeda375d9482dd022aea2d36d08b0784c7ebd
Nicolas Galland <61990668+nicolasgallandpro@users.noreply.github.com>
2021-04-21 10:56:00 +0200

    ajout de ripgrep

--- new_arch_server.sh
+++ new_arch_server.sh
@@ -1,4 +1,4 @@
 pacman -Sy
-pacman -S --noconfirm git docker docker-compose caddy curl tree starship nano
+pacman -S --noconfirm git docker docker-compose caddy curl tree starship nano ripgrep
 git config --global init.defaultBranch main
 systemctl start docker
