c48fd5ebef723e5fdbb76e26eee9407f81ac8a05
Nicolas Galland <61990668+nicolasgallandpro@users.noreply.github.com>
2021-06-02 11:02:14 +0200

    ajout de wget

--- new_arch_server.sh
+++ new_arch_server.sh
@@ -1,4 +1,4 @@
 pacman -Sy
-pacman -S --noconfirm git docker docker-compose caddy curl tree starship nano ripgrep
+pacman -S --noconfirm git docker docker-compose caddy curl tree starship nano ripgrep wget
 git config --global init.defaultBranch main
 systemctl start docker
