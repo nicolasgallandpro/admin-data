c2dcaaff21faba2f61317ed84bded10624ce4541
Nicolas Galland <61990668+nicolasgallandpro@users.noreply.github.com>
2021-10-25 20:45:48 +0200

    ajout peco et exa

--- new_arch_server.sh
+++ new_arch_server.sh
@@ -1,4 +1,4 @@
 pacman -Sy
-pacman -S --noconfirm git docker docker-compose caddy curl tree starship nano ripgrep wget
+pacman -S --noconfirm git docker docker-compose caddy curl tree starship nano ripgrep wget peco exa
 git config --global init.defaultBranch main
 systemctl start docker
