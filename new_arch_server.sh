057e247cc899ef4203fdb77f2e8b946fdb9f94df
Nicolas Galland <61990668+nicolasgallandpro@users.noreply.github.com>
2022-08-22 23:29:12 +0200

    Update new_arch_server.sh

--- new_arch_server.sh
+++ new_arch_server.sh
@@ -1,4 +1,4 @@
 pacman -Sy
-pacman -S --noconfirm git docker docker-compose curl tree starship nano ripgrep wget peco exa
+pacman -S --noconfirm git docker docker-compose curl tree starship nano ripgrep wget peco exa pacman unzip
 git config --global init.defaultBranch main
 systemctl start docker
